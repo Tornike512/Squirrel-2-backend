ce28fec51c9f62d437554261e819a297b15e3969
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-19 20:11:09 +0400

    Drop: Socket.io

--- index.mjs
+++ index.mjs
@@ -1,14 +1,10 @@
 import express from "express";
-import http from "http";
-import { Server } from "socket.io";
 import dotenv from "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
-
-// Connect to Database using mongoDB
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -23,13 +19,3 @@ mongoose
   });
 
 // Set Up Socket.io
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST", "PUT"] },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected:${socket.id}`);
-});
