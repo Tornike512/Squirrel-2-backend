cf0381d4ef76c354c6b19ce3e0ad1ce952476809
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-22 18:06:39 +0400

    Add: Ip If it doesn't exist in database

--- Routes/IpAddress.mjs
+++ Routes/IpAddress.mjs
@@ -10,15 +10,21 @@ sendIpPost.use(requestIp.mw());
 sendIpPost.post("/api/update", async (req, res) => {
   const ipAddress = req.clientIp;
 
-  try {
-    const newIpAddress = new statement({ ipAddress });
-    await newIpAddress.save();
+  const ipArray = await statement.find({}, { ipAddress: 1, _id: 0 });
 
-    res.status(201).send({ ipAddress });
-  } catch (error) {
-    res
-      .status(404)
-      .json({ message: "Error Sending Post Request For Ip Address" });
+  if (ipArray.includes(ipAddress)) {
+    try {
+      const newIpAddress = new statement({ ipAddress });
+      await newIpAddress.save();
+
+      res.status(201).send({ ipAddress });
+    } catch (error) {
+      res
+        .status(404)
+        .json({ message: "Error Sending Post Request For Ip Address" });
+    }
+  } else {
+    return;
   }
 });
 
